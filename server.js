349f2df8d2d5dc31c31435194bda5c305a6d9675
hamid154 <sonu808360@gmail.com>
2026-04-06 04:59:22 +0530

    Fix MongoDB connection options and add .env to gitignore

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 // Signup Route
