354d0202fb5cd58b92b92b0cdd9595cf738bbcad
hamid154 <sonu808360@gmail.com>
2026-04-09 23:25:33 +0530

    Fix: Removed next() from async pre-save hook for Mongoose compatibility

--- models/User.js
+++ models/User.js
@@ -9,16 +9,12 @@ const UserSchema = new mongoose.Schema({
 
 
 // Pre-save hook to hash password
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
+
 
 // Method to compare password
 UserSchema.methods.comparePassword = async function (enteredPassword) {
